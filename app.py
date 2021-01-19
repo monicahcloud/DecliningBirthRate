920af4cc155166ab7cccbe5c6117f9c46e453190
Monicah Cloud <monicahcloud@gmail.com>
2021-01-18 19:04:00 -0500

    Update 4

--- app.py
+++ app.py
@@ -1,43 +1,98 @@
 #################################################
-# Database Setup
+# Flask and Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
+# Import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-# Save reference to the table
-
-
-# 1. import Flask
-from flask import Flask
-
-import datetime as dt
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from sqlalchemy.sql import text
-
-# 2. Create an app, being sure to pass __name__
+# Create an app, being sure to pass __name__
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///birthRate.db'
+db = SQLAlchemy(app)
 
+# Design database
+class birth(db.Model):
+    state = db.Column(db.String, primary_key=True)
+    county = db.Column(db.String(128), nullable=False, default='N/A')
+    year = db.Column(db.Integer, nullable=False, default='N/A')
+    births = db.Column(db.Integer,  nullable=False, default='N/A')
+    birthrate = db.Column(db.Integer,  nullable=False, default='N/A')
+    fertilityrate = db.Column(db.Integer,  nullable=False, default='N/A')
 
-# 3. Define what to do when a user hits the index route
+    def __repr__(self):
+        return 'birth' + str(self.state)
+
+class fertility_info(db.Model):
+    state = db.Column(db.String, primary_key=True)
+    year = db.Column(db.Integer, nullable=False, default='N/A')
+    race = db.Column(db.String,  nullable=False, default='N/A')
+    births = db.Column(db.Integer,  nullable=False, default='N/A')
+    fertilityrate = db.Column(db.Integer,  nullable=False, default='N/A')
+    femalePopulation = db.Column(db.Integer,  nullable=False, default='N/A')
+    def __repr__(self):
+        return 'fertility_info' + str(self.state)
+
+class birth_rate(db.Model):
+    state = db.Column(db.String, primary_key=True)
+    year = db.Column(db.Integer, nullable=False, default='N/A')
+    race = db.Column(db.String,  nullable=False, default='N/A')
+    age = db.Column(db.Integer, nullable=False, default= "N/A")
+    births = db.Column(db.Integer,  nullable=False, default='N/A')
+    femalePopulation = db.Column(db.Integer,  nullable=False, default='N/A')
+    fertilityrate = db.Column(db.Integer,  nullable=False, default='N/A')
+    totalPopulation = db.Column(db.Integer, nullable=False, default='N/A')
+    birthrate = db.Column(db.Integer,  nullable=False, default='N/A')
+    def __repr__(self):
+        return 'birth_rate' + str(self.state)
+ 
+class moms_education(db.Model):
+    state = db.Column(db.String, primary_key=True)
+    year = db.Column(db.Integer, nullable=False, default='N/A')
+    race = db.Column(db.String,  nullable=False, default='N/A')
+    age = db.Column(db.Integer, nullable=False, default= "N/A")
+    births = db.Column(db.Integer,  nullable=False, default='N/A')
+    education = db.Column(db.String,  nullable=False, default='N/A')
+    
+    def __repr__(self):
+        return 'moms_education' + str(self.state)
+
+class infant(db.Model):
+    state = db.Column(db.String, primary_key=True)
+    year = db.Column(db.Integer, nullable=False, default='N/A')
+    sex = db.Column(db.String,  nullable=False, default='N/A')
+    plurality = db.Column(db.String, nullable=False, default= "N/A")
+    mom_age = db.Column(db.Integer,  nullable=False, default='N/A')
+    births = db.Column(db.Integer,  nullable=False, default='N/A')
+
+    def __repr__(self):
+        return 'infant' + str(self.state)
+
+# Define what to do when a user hits the index route
 @app.route("/")
-def home():
-    print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
+def index():
+    return render_template("index.html")
 
-
-# 4. Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+    return render_template("about.html")  
 
-# 5. Define main behavior
+@app.route("/race")
+def race():
+    return render_template("race.html")  
+
+@app.route("/state")
+def state():
+    return render_template("state.html")       
+
+@app.route("/total")
+def total():
+    return render_template("total.html")       
+
+@app.route("/age")
+def age():
+    return render_template("age.html")
+
+# Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
