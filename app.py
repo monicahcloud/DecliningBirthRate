7998d6d9af86ae08a15a0b7f5779ace8126d689c
Monicah Cloud <monicahcloud@gmail.com>
2021-01-21 00:11:26 -0500

    update 8

--- app.py
+++ app.py
@@ -8,33 +8,42 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://mpiljlhpdduyfa:2332f5bec0021e7c8a94d4f2450748ccf8aeaf1a965c62cc3d3885a838a5e316@ec2-52-22-238-188.compute-1.amazonaws.com:5432/d29t1m9qk328vp"
+
 db = SQLAlchemy(app)
 
-# Design database
-
+# Map to database
+class Total(db.Model):
+    __tablename__ = "total"
+    state = db.Column('state', db.Unicode, primary_key=True)
+    year = db.Column('year', db.Unicode)
+    race = db.Column('race', db.Unicode)
+    births = db.Column('births', db.Unicode)
+    femalepop = db.Column('femalepop', db.Unicode)
+    fertilityrate = db.Column('fertilityrate', db.Unicode) 
+    
 
 # Define what to do when a user hits the index route
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 
-@app.route("/race")
-def race():
-    return render_template("race.html")  
+# @app.route("/race")
+# def race():
+#     return render_template("race.html")  
 
-@app.route("/state")
-def state():
-    return render_template("state.html")       
+# @app.route("/state")
+# def state():
+#     return render_template("state.html")       
 
-@app.route("/total")
-def total():
-    return render_template("total.html")       
+# @app.route("/total")
+# def total():
+#     return render_template("total.html")       
 
-@app.route("/age")
-def age():
-    return render_template("age.html")
+# @app.route("/age")
+# def age():
+#     return render_template("age.html")
 
-# Define main behavior
-if __name__ == "__main__":
-    app.run(debug=True)
+# # Define main behavior
+# if __name__ == "__main__":
+#     app.run(debug=True)
