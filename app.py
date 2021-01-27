c8955fa57ef62b382513fc332d8458fb252f5041
Monicah Cloud <monicahcloud@gmail.com>
2021-01-27 00:34:59 -0500

    Final Day

--- app.py
+++ app.py
@@ -7,58 +7,61 @@ from flask import Flask, render_template, request, redirect, jsonify
 import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, func
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import Sequence
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy.sql import text
-import json
 
-#Connect to database
+#Database setup
+
 connection_string = "postgresql://postgres:postgres@localhost:5432/BirthRate"
 engine = create_engine(f'{connection_string}')
 
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+birthrate = Base.classes.birthrate
+births_table = Base.classes.births
+
+
+#Flask Set up
 app = Flask(__name__)
-
-db = SQLAlchemy(app)
-
-#inspect the database 
-from sqlalchemy import inspect
-inspector = inspect(engine)
-
-# Get names of tables in database
-# print(inspector.get_table_names())
-
-#Executing Raw SQL Statements
-con = engine.connect()
-rs = con.execute("SELECT * FROM births")
-
-# print(rs.fetchall())
-
-
+# Flask routes
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/data")
+def data():
+    return render_template("data.html")   
+
 
 @app.route("/births")
 def births():
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Return a list of data including the state, year, race and births"""
+    # Query all data
+    results = session.query(births_table.state, births_table.year, births_table.race, births_table.births).all()
+# print(results)
+
+    session.close()
+
     data = []
-    for info in (rs.fetchall()):
-        # print(info)      
-        data.append({
-            "state": info["state"],
-            "year": info["year"],
-            'race': info['race'],
-            "births": info['births'],
-            # 'total_pop':info['total_pop'] ,
-            # 'birth_rate':info['birth_rate'],
-            # 'female_pop':info['female_pop'],
-            # 'fertility_rate':info['fertility_rate'],
-        })
-           
-    return jsonify(data=data)
+    for state, year, race, birth in results:
+    # print(state, year, race, births)
+# Create a dictionary from the row data and append to a list of all_passengers
+        info = {}
+        info['state'] = state
+        info['year'] = year
+        info['race'] = race
+        info['births'] = birth
+        data.append(info)
+# print(data)
 
-
-if __name__ == "__main__":
+    return jsonify(data)
+    
+if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,151 +1,12 @@
-// <<<<<<< HEAD
-//   // Dropdown select options
-  
-//   let selector = d3.select("#selDataset");
-//   // Lest of sample names to populate
-  
-// =======
-
-//   // Grab a reference to the dropdown select element
-//   let selector = d3.select("#selDataset");
-//   // Use the list of sample names to populate the select options
-// >>>>>>> b8835e029160940ea297e255437dbf0fb78e3d0e
-//   d3.json("samples.json").then((data) => {
-//     let names = data.names;
-//     names.forEach((sample) => {
-//       selector
-//         .append("option")
-//         .text(sample)
-//         .property("value", sample);
-//     });
-// <<<<<<< HEAD
-  
-// =======
-// >>>>>>> b8835e029160940ea297e255437dbf0fb78e3d0e
-//     // Use the first sample from the list to build the initial plots
-//     let firstSample = names[0];
-    
-//     buildCharts(firstSample);
-//     metadata(firstSample);
-//   });
-
-// function metadata(sample) {
-//   d3.json("samples.json").then((data) => {
-//     let metadata = data.metadata;
-// <<<<<<< HEAD
-  
-//     // Loop through array of objects then each object
-//         let filtered = metadata.filter((item) => item.id == sample);
-//         console.log(filtered[0]);
-  
-//         // Then, select the unordered list element by class name
-//         let list = d3.select("#sample-metadata");
-  
-//         // First, clear out any existing data
-//         list.html("");
-  
-// =======
-//         // Loop through array of objects then each object
-//         let filtered = metadata.filter((item) => item.id == sample);
-//         console.log(filtered[0]);
-//         // Then, select the unordered list element by class name
-//         let list = d3.select("#sample-metadata");
-//         // First, clear out any existing data
-//         list.html("");
-// >>>>>>> b8835e029160940ea297e255437dbf0fb78e3d0e
-//         // Display the sample metadata, i.e., an individual's demographic information.
-//         // Append the id data to the dropdown menu
-//         list.append("li").text(filtered[0].id);
-//         list.append("li").text(filtered[0].ethnicity);
-//         list.append("li").text(filtered[0].gender);
-//         list.append("li").text(filtered[0].age);
-//         list.append("li").text(filtered[0].location); 
-//   });
-// }
-
-// function buildCharts(sample) {
-// d3.json("samples.json").then((data) => {
-//   let samples = data.samples;
-//   let sample_id = samples.filter(item => item.id == sample);
-//   let result = sample_id[0];
-  
-//   let otu_ids = result.otu_ids;
-//   let otu_labels = result.otu_labels;
-//   let sample_values = result.sample_values;
-//   let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-
-// <<<<<<< HEAD
-//   // Bar Chart
-// =======
-//   // Build BarChart
-// >>>>>>> b8835e029160940ea297e255437dbf0fb78e3d0e
-   
-//   let trace1 = [
-//   {
-//       y: yticks,
-//       x: sample_values.slice(0, 10).reverse(),
-//       text: otu_labels.slice(0, 10).reverse(),
-//       type: "bar",
-//       orientation: "h",
-//   }
-//   ];
-//   let barLayout = {
-//   title: "Top 10 Bacteria Cultures Found",
-//   margin: { t: 30, l: 150 }
-//   };
-//   Plotly.newPlot("bar", trace1, barLayout);
-
-// <<<<<<< HEAD
-//   // Bubble Chart
-// =======
-//   // Build a Bubble Chart
-// >>>>>>> b8835e029160940ea297e255437dbf0fb78e3d0e
-  
-//   let trace2 = [
-//   {
-//       x: otu_ids,
-//       y: sample_values,
-//       text: otu_labels,
-//       mode: "markers",
-//       marker: {
-//       size: sample_values,
-//       color: otu_ids,
-//       colorscale: "Jet"
-//       }
-//   }
-// ];
-
-//   let bubbleChartLayout = {
-//     title: "Bacteria Cultures Per Sample",
-//     margin: { t: 0 },
-//     hovermode: "closest",
-//     xaxis: { title: "OTU ID" },
-//     margin: { t: 30}
-//     };
- 
-//   Plotly.newPlot("bubble", trace2, bubbleChartLayout);
-  
-// });
-// }
-
-// function optionChanged(newSample) {
-
-//   // Fetch new data each time a new sample is selected
-// buildCharts(newSample);
-// metadata(newSample);
-// }
-
-// // Fetch new data each time a new sample is selected
-// buildCharts(newSample);
-// metadata(newSample);
-// }
-
 // Javascript for data.html
-d3.json("/births")
+const loc = window.location.pathname
+
+if (loc==="/data"){
+    d3.json("/births")
   .then(data => {
-    const dataset = data.data;
     console.log(data)
-    const tbody = d3.select("#tbody");
+    const dataset = data;
+    const tbody = document.querySelector("tbody");
     dataset.map(item => {
       tbody.innerHTML += `
       <tr>
@@ -158,3 +19,29 @@ d3.json("/births")
     })
   })
 
+}
+
+
+
+
+
+//   // from data.js
+//   if (loc==="/data"){
+//     d3.json("/births")
+//   .then(data => {
+//     console.log(data)
+//     const dataset = data;
+//     const tbody = document.querySelector("tbody");
+//     dataset.map(item => {
+//       tbody.innerHTML += `
+//       <tr>
+//         <td>${item.state}</td>
+//         <td>${item.year}</td>
+//         <td>${item.race}</td>
+//         <td>${item.births}</td>
+//        </tr>
+//       `
+//     })
+//   })
+
+// }
